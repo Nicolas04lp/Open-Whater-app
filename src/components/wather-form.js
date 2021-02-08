26e1bdffea408b016495b365db1c0ad6ecbc79ed
Nicolás Leon <nicolsnicolasleon@gmail.com>
2021-02-08 18:59:54 -0500

    commit inicial 1

--- src/components/wather-form.js
+++ src/components/wather-form.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const WatherForm = (props) => {
     return(
+        
         <div className="card  card-body">
+            <h2 className="p-3">Select your City and Country</h2>
             <form onSubmit={props.getWather}>
                 <div className="form-group"> 
                     <input type="text" name="city" placeholder="Your City Name" className="form-control"/>
